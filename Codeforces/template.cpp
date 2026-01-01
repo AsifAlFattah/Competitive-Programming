be927b5aaa0670042fee6eb7455d7b33dcae8af7
AsifAlFattah <asif.alfattah.2@gmail.com>
2026-01-01 20:40:48 +0600

    cp template

---
+++ Codeforces/template.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll t=1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
